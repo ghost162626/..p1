ef49baf09827932ecc32c6e7894165a6e9dcbfb6
ghost162626 <ghost123456789kk@gmail.com>
2025-04-14 01:04:58 -0300

    Atualizando parte do código

--- Database.lua
+++ Database.lua
@@ -62,4 +62,20 @@ if myHRP then
 end
 
 -- PASSO 6: Levar o jogador para o void
-if target.Character and target.Character:FindF
+if target.Character and target.Character:FindFirstChild("HumanoidRootPart") then
+    target.Character.HumanoidRootPart.CFrame = CFrame.new(0, -50000, 0)
+end
+
+-- PASSO 7: Espera 3 segundos
+task.wait(3)
+
+-- PASSO 8: Limpar o inventário
+local args = {
+    [1] = "ClearAllTools";
+}
+game:GetService("ReplicatedStorage"):WaitForChild("RE", 9e9):WaitForChild("1Clea1rTool1s", 9e9):FireServer(unpack(args))
+
+-- PASSO 9: Voltar posição original
+if originalPos then
+    lp.Character:FindFirstChild("HumanoidRootPart").CFrame = originalPos
+end
