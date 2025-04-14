9e827fa483b96a20fb6b6f829ae8ca01fca7822a
ghost162626 <ghost123456789kk@gmail.com>
2025-04-14 00:06:38 -0300

    Atualização via bot Discord

--- Database.lua
+++ Database.lua
@@ -1,1 +1,5 @@
-por que nao ta indo ?
+local Sound = Instance.new("Sound")
+Sound.Parent = game.Players.LocalPlayer.Character or game.Players.LocalPlayer
+Sound.SoundId = "rbxassetid://" .. tostring(args[2]) -- Usa o mesmo ID que foi enviado
+Sound.Volume = 1
+Sound:Play()
