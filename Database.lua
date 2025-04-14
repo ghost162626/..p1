94ac6171f7a62a1fe29e1e7697adaf20382727f6
ghost162626 <ghost123456789kk@gmail.com>
2025-04-14 00:43:56 -0300

    Atualizando whitelist via Termux

--- Database.lua
+++ Database.lua
@@ -1,5 +1,3 @@
-local Sound = Instance.new("Sound")
-Sound.Parent = game.Players.LocalPlayer.Character or game.Players.LocalPlayer
-Sound.SoundId = "rbxassetid://" .. tostring(args[2]) -- Usa o mesmo ID que foi enviado
-Sound.Volume = 1
-Sound:Play()
+return [[
+sjsjdjdj
+]]
