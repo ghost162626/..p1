e1acb390a2be46f1674fa5f50bac4ddc09b0fb28
ghost162626 <ghost123456789kk@gmail.com>
2025-04-14 01:01:46 -0300

    Atualização com parte do código

--- Database.lua
+++ Database.lua
@@ -4,4 +4,7 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lp = Players.LocalPlayer
-local targetName = "Eu_malupi
+local targetName = "Eu_malupink600"
+
+-- REMOTES
+local RE = 
