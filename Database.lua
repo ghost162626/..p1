776955c816544a99ca85419275380b3347c63fab
ghost162626 <ghost123456789kk@gmail.com>
2025-04-14 01:02:52 -0300

    Atualização com parte do código

--- Database.lua
+++ Database.lua
@@ -57,4 +57,5 @@ until isPlayerSitting(target)
 
 -- PASSO 5: Prevenir que você morra - Mover seu personagem para uma posição segura
 local myHRP = lp.Character and lp.Character:FindFirstChild("HumanoidRootPart")
-if m
+if myHRP then
+    myHRP.CFrame = C
